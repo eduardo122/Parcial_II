b10cb165a1a99c581e3c99c8241c62fe4245073e
manuelitoman2 <manuelitoman2@gmail.com>
2019-11-20 02:27:54 -0400

    Commit repetido -Agregue una validacion en el DropDownList para que no se pueda acceder a un integrante vacio -Agregue un enlance para volver a la vista de asignaturas

--- Examen_Practico/Examen_Practico/Controllers/HomeController.cs
+++ Examen_Practico/Examen_Practico/Controllers/HomeController.cs
@@ -131,22 +131,29 @@ namespace Examen_Practico.Controllers
             List<profesor> listaProfesor = sd.profesors.ToList();
             List<imparte> listaImparte = sd.impartes.ToList();
 
-            ViewData["AsignaturasCursadas"] = from cur in listacursa
-                                              join i in listaintegrante on cur.id_estudiante equals i.id into table1
-                                              from i in table1.DefaultIfEmpty() where i.id == integrante.id
-                                              join asign in listaasignatura on cur.id_materia equals asign.id into table2
-                                              from asign in table2.DefaultIfEmpty()
-                                              join imp in listaImparte on asign.id equals imp.id_materia into table3
-                                              from imp in table3.DefaultIfEmpty()
-                                              join prof in listaProfesor on imp.id_profesor equals prof.id into table4
-                                              from prof in table4.DefaultIfEmpty()
+            if (integrante.id == 0)
+            {
+                return Redirect("Asignaturas");
+            }
+            else
+            {
+                ViewData["AsignaturasCursadas"] = from cur in listacursa
+                                                  join i in listaintegrante on cur.id_estudiante equals i.id into table1
+                                                  from i in table1.DefaultIfEmpty()
+                                                  where i.id == integrante.id
+                                                  join asign in listaasignatura on cur.id_materia equals asign.id into table2
+                                                  from asign in table2.DefaultIfEmpty()
+                                                  join imp in listaImparte on asign.id equals imp.id_materia into table3
+                                                  from imp in table3.DefaultIfEmpty()
+                                                  join prof in listaProfesor on imp.id_profesor equals prof.id into table4
+                                                  from prof in table4.DefaultIfEmpty()
 
 
-                                              select new AsignaturasCursadas { listaAsignaturas = asign, listaIntegrantes = i, listaCursa = cur, listaImparte = imp, listaProfesores=prof };
+                                                  select new AsignaturasCursadas { listaAsignaturas = asign, listaIntegrantes = i, listaCursa = cur, listaImparte = imp, listaProfesores = prof };
 
-            return View( ViewData["AsignaturasCursadas"]);
+                return View(ViewData["AsignaturasCursadas"]);
+            }
         }
-
 
 
 
