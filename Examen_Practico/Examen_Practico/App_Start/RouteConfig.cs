ee00c53f5f6eda08c1e9494b47c98c94f6e3be48
ferrerasldf@hotmail.com <ferrerasldf@hotmail.com>
2019-11-20 00:52:54 -0400

    -Se elemino el controlador Lector. -Se Edito el ActionResult Index. -Se creo un ActionResult Post para el action index. -Se instalo el Paquete Nugget ExcelDataReader -Se creo la funcionalidad del Lector de Archivos de Excel. -Se creo la Vista Inicio -Se cambio la ruta por defecto de inicio de aplicacion de la vista Inicio

--- Examen_Practico/Examen_Practico/App_Start/RouteConfig.cs
+++ Examen_Practico/Examen_Practico/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Examen_Practico
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Inicio", id = UrlParameter.Optional }
             );
         }
     }
