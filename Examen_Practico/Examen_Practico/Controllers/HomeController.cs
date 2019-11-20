5723f23ffd99bb1c60e6ff8b2fd851d3bd50ea81
manuelitoman2 <manuelitoman2@gmail.com>
2019-11-20 02:15:49 -0400

    -Tuve que volver a subir la solucion porque se corrompieron algunos archivos

--- Examen_Practico/Examen_Practico/Controllers/HomeController.cs
+++ Examen_Practico/Examen_Practico/Controllers/HomeController.cs
@@ -131,10 +131,6 @@ namespace Examen_Practico.Controllers
             List<profesor> listaProfesor = sd.profesors.ToList();
             List<imparte> listaImparte = sd.impartes.ToList();
 
-            if (integrante.id == 0 ) {
-                return Redirect("Asignaturas");
-            }
-            else { 
             ViewData["AsignaturasCursadas"] = from cur in listacursa
                                               join i in listaintegrante on cur.id_estudiante equals i.id into table1
                                               from i in table1.DefaultIfEmpty() where i.id == integrante.id
@@ -150,7 +146,7 @@ namespace Examen_Practico.Controllers
 
             return View( ViewData["AsignaturasCursadas"]);
         }
-        }
+
 
 
 
