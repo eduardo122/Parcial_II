bddc258c7918398ee79144cde2ae99fdb4190587
manuelitoman2 <manuelitoman2@gmail.com>
2019-11-19 22:37:46 -0400

    -Se creo la base de datos en MS SQL SERVER -Se creo la conexion a la base de datos SQL utilizando Entity Framework con el modelo DataseFirst -Se edito la vista Asignaturas agregando un DropDownList el cual obtiene la informacion desde la base de datos SQL y mediante un POST envia el ID del usuario selccionado al ActionResult Contact -Se creo el modelo AsignaturasCursadas -Se agrego la funcionalidad de las asignaturas mostrando las asignaturas cursadas por cada integrante del grupo

--- Examen_Practico/Examen_Practico/Controllers/HomeController.cs
+++ Examen_Practico/Examen_Practico/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Examen_Practico.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace Examen_Practico.Controllers
 {
@@ -20,15 +22,43 @@ namespace Examen_Practico.Controllers
             return View();
         }
         [Authorize]
-        public ActionResult Contact()
+        public ActionResult Contact(integrante integrante)
         {
-            ViewBag.Message = "Your contact page.";
+            Examen_ParcialEntities3 sd = new Examen_ParcialEntities3();
+            List<integrante> listaintegrante = sd.integrantes.ToList();
+            List<Asignatura> listaasignatura = sd.Asignaturas.ToList();
+            List<cursa> listacursa = sd.cursas.ToList();
+            List<profesor> listaProfesor = sd.profesors.ToList();
+            List<imparte> listaImparte = sd.impartes.ToList();
 
-            return View();
+            ViewData["AsignaturasCursadas"] = from cur in listacursa
+                                              join i in listaintegrante on cur.id_estudiante equals i.id into table1
+                                              from i in table1.DefaultIfEmpty() where i.id == integrante.id
+                                              join asign in listaasignatura on cur.id_materia equals asign.id into table2
+                                              from asign in table2.DefaultIfEmpty()
+                                              join imp in listaImparte on asign.id equals imp.id_materia into table3
+                                              from imp in table3.DefaultIfEmpty()
+                                              join prof in listaProfesor on imp.id_profesor equals prof.id into table4
+                                              from prof in table4.DefaultIfEmpty()
+
+
+                                              select new AsignaturasCursadas { listaAsignaturas = asign, listaIntegrantes = i, listaCursa = cur, listaImparte = imp, listaProfesores=prof };
+
+            return View( ViewData["AsignaturasCursadas"]);
         }
+
+
+
+
+
         [Authorize]
         public ActionResult Asignaturas()
         {
+
+            Examen_ParcialEntities3 myEntity = new Examen_ParcialEntities3();
+            var ListaEstudiantes = myEntity.integrantes.ToList();
+            SelectList list = new SelectList(ListaEstudiantes, "ID", "Nombre");
+            ViewBag.Integrantes = list;
             return View();
         }
         [Authorize]
---
+++ Examen_Practico/Examen_Practico/Models/AsignaturasCursadas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen_Practico.Models
+{
+    public class AsignaturasCursadas
+    {
+        public integrante listaIntegrantes { get; set; }
+        public Asignatura listaAsignaturas { get; set; }
+        public cursa listaCursa { get; set; }
+        public profesor listaProfesores { get; set; }
+        public imparte listaImparte { get; set; }
+    }
+}
